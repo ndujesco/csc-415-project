15df9b512328e95294a2d468f01ba1b69a795c85
ndujesco <92054718+ndujesco@users.noreply.github.com>
2025-03-11 06:58:38 +0100

    format

--- backend/src/serializers/Order.ts
+++ backend/src/serializers/Order.ts
@@ -2,34 +2,33 @@ import 'reflect-metadata'
 import { IsInt, IsString, IsArray, Min, Length, IsOptional, IsEmail } from "class-validator";
 
 class CreateOrderDTO {
-  @IsInt()
-  @Min(1)
-  amount!: number;
+    @IsInt()
+    @Min(1)
+    amount!: number;
 
-  @IsString()
-  @Length(5, 100)
-  address!: string;
+    @IsString()
+    @Length(5, 100)
+    address!: string;
 
-  @IsArray()
-  order!: string[];
+    @IsArray()
+    order!: string[];
 
-  @IsOptional()
-  @IsEmail()
-  email?: string;
+    @IsOptional()
+    @IsEmail()
+    email?: string;
 
-  @IsOptional()
-  @IsString()
-  @Length(3, 20)
-  status?: string; 
+    @IsOptional()
+    @IsString()
+    @Length(3, 20)
+    status?: string;
 }
 
 
 class UpdateOrderDTO extends CreateOrderDTO {
     @IsEmail()
-    email!: string; 
+    email!: string;
+}
 
-  }
-  
 
 
 export { CreateOrderDTO, UpdateOrderDTO };
